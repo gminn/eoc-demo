e0d41c68f9be81fe6772fbe4f2848e5be29ab6ed
Gillian Minnehan <gminn@umich.edu>
2023-03-26 17:08:58 -0400

    Complete counter demo in app init

--- eoc_demo/app.c
+++ eoc_demo/app.c
@@ -1,6 +1,6 @@
 /**
  * @file app.c
- * @author Silicon Labs, edited by Gillian Minnehan
+ * @author Silicon Labs, edited by Gillian Minnehan for EOC 2023 Talk Demo
  * @brief Top level application functions
  * @version 0.1
  * @date 2023-03-17
@@ -20,13 +20,30 @@
 
 #include "counter.h"
 
+static void counter_demo(bool call_printf);
+
 /**
  * @brief Initializes the application
  *
  */
 void app_init(void) {
+    // Counter demo
+    counter_demo(true);   // call printf
+    counter_demo(false);  // don't call printf
+}
+
+/**
+ * @brief Demonstrate counting cycles with a call to printf
+ *
+ * @param call_printf whether to call printf (true) or not (false)
+ */
+static void counter_demo(bool call_printf) {
     counter_init();
     start_counter(true);
-    printf("Testing counter!\n");
+
+    if (call_printf) {
+        printf("Testing counter!\n");
+    }
+
     float elapsed_time_us = get_elapsed_time();
 }
